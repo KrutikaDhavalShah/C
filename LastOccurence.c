70e5f66799561ae6f08d2e00105c6ba010b90d1b
Krutika Dhaval Shah <119673029+KrutikaDhavalShah@users.noreply.github.com>
2022-12-07 02:29:17 +0530

    Add files via upload

---
+++ LastOccurence.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+// Step 5 : Perform the operation on array
+int CheckLastOccurance(int Arr[], int iSize, int iNo)
+{
+   int iCnt = 0;
+
+    for(iCnt = iSize - 1; iCnt <= 0; iCnt--)
+    {
+      if(Arr[iCnt] == iNo)
+      {
+        break;
+      }
+    }  
+      if(iCnt == -1)
+      {
+        return -1;
+      }
+      else
+      {
+        return iCnt;
+      }
+}
+
+int main()
+{
+    int *ptr = NULL;
+    int iLength = 0, i = 0, iValue = 0, iRet = 0;
+
+    // Step 1 : Accept size of array
+    printf("Enter number of elements : \n");
+    scanf("%d",&iLength);
+
+    // Step 2 : Allocate memory for array
+    ptr = (int *)malloc(iLength * sizeof(int));
+
+    // Step 3 : Accept the elements of array
+    printf("Enter the elements : \n");
+
+    for(i = 0 ;i < iLength; i++)
+    {
+        scanf("%d",&ptr[i]);
+    }
+    printf("Enter the element to findout the last occurence:\n");
+    scanf("%d", &iValue);
+
+    // Step 4 : Call the function
+    iRet = CheckLastOccurance(ptr,iLength, iValue);
+
+    if(iRet == -1)
+    {
+      printf("%d is not present in the array\n",iValue );
+    }
+    else
+    {
+      printf("%d is occured in the array at the last occurence at the index %d\n", iValue,iRet);
+    }
+
+    // Step 6 : Deallocate the memory
+    free(ptr);
+
+    return 0;
+}
+
+//Time complexity:
+//Best case 1
+
+//Worst case N
+
+//Average Case - 1 to N
+
+
+
