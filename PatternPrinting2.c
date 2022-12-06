4d12fcb590ce6307a313a3b50c6d4cb3dc7a8f01
Krutika Dhaval Shah <119673029+KrutikaDhavalShah@users.noreply.github.com>
2022-12-07 02:33:07 +0530

    Add files via upload

---
+++ PatternPrinting2.c
@@ -0,0 +1,36 @@
+// 4
+
+//{1   2   3   4 } {*   *   *   *};  serial loop -> loop one below other
+
+//Time Complexity : 2N
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+  int iCnt = 0;
+  if(iNo < 0)
+  {
+    iNo = -iNo;
+  }
+  for(iCnt = 1; iCnt <= iNo; iCnt++)
+  {
+    printf("%d\t",iCnt);              
+  }
+   for(iCnt = 1; iCnt <= iNo; iCnt++)
+  {
+    printf("*\t");             
+  }
+}
+
+int main()
+{
+  int iValue = 0;
+
+  printf("Enter number\n");
+  scanf("%d",&iValue);
+
+  Display(iValue);
+
+  return 0;
+}
